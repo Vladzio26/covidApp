044ee6e6c970b1653363ff7314f856f16b581833
Mykola Kravchenko <vladkravchenko51@gmail.com>
2020-04-18 18:27:35 +0300

    Updates

---
+++ precache-manifest.bc7e7035b4635ca5fea2134aa574b6a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c87eff2a7e5643ac8ac929726b478f",
+    "url": "/Vladzio26/covidApp/index.html"
+  },
+  {
+    "revision": "d473ef2ac46960ab18f8",
+    "url": "/Vladzio26/covidApp/static/css/main.6798af72.chunk.css"
+  },
+  {
+    "revision": "b6b3f4b5b0a17d3e1780",
+    "url": "/Vladzio26/covidApp/static/js/2.28f90c4d.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/Vladzio26/covidApp/static/js/2.28f90c4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d473ef2ac46960ab18f8",
+    "url": "/Vladzio26/covidApp/static/js/main.329d26c1.chunk.js"
+  },
+  {
+    "revision": "1602e9e220b7cf217b49",
+    "url": "/Vladzio26/covidApp/static/js/runtime-main.a1319f78.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Vladzio26/covidApp/static/media/image.d7265326.png"
+  }
+]);
